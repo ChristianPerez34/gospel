b7e24601565c853032660eed60754cb608e3d226
Christianperez34 <christian.perez34@outlook.com>
2026-05-18 22:21:51 -0400

    fix: re-fetch available models when providers change so newly configured providers' models appear

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -85,7 +85,7 @@ export function AppShell() {
         setStatus("idle");
       }
     })();
-  }, []);
+  }, [providers]);
 
   useEffect(() => {
     const models = buildModelOptions(availableModels, providers);
